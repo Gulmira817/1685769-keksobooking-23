2cdb6ea119334364e2d9e6db216a051351b570ae
Gulmira817 <38330620+Gulmira817@users.noreply.github.com>
2021-06-04 17:25:05 +0400

    Downloaded eslint

--- js/main.js
+++ js/main.js
@@ -1,37 +1,28 @@
-let randomFunction=(startRandomNumber,toRandomNumber)=>{
-    if(startRandomNumber>=0 && toRandomNumber>=0)
-    {
-        if(startRandomNumber<=toRandomNumber){
-            return Math.ceil(Math.random()*200);
-        }else{
-            console.log("startRandomNumber must be less than toRandomNumber");
-        }
-
-    }else{
-        console.log("startRandomNumber and toRandomNumber must be positive or equal to zero");
-
+const randomFunction = (startRandomNumber, toRandomNumber) => {
+  if (startRandomNumber >= 0 && toRandomNumber >= 0) {
+    if (startRandomNumber <= toRandomNumber) {
+      return Math.ceil(Math.random() * 200)
+    } else {
+      console.log('startRandomNumber must be less than toRandomNumber')
     }
-
+  } else {
+    console.log('startRandomNumber and toRandomNumber must be positive or equal to zero')
+  }
 }
 
-
-
-let randomFunctionDouble=(startRandomNumber,toRandomNumber,count)=>{
-    if(startRandomNumber>=0 && toRandomNumber>=0)
-    {
-        if(startRandomNumber<=toRandomNumber){
-            let result=(Math.ceil(Math.random()));
-            count= Math.pow(10, count);;
-            result=result/count;
-            return result ;
-        }else{
-            console.log("startRandomNumber must be less than toRandomNumber");
-        }
-
-    }else{
-        console.log("startRandomNumber and toRandomNumber must be positive or equal to zero");
-
+const randomFunctionDouble = (startRandomNumber, toRandomNumber, count) => {
+  if (startRandomNumber >= 0 && toRandomNumber >= 0) {
+    if (startRandomNumber <= toRandomNumber) {
+      let result = (Math.ceil(Math.random()))
+      count = Math.pow(10, count)
+      result = result / count
+      return result
+    } else {
+      console.log('startRandomNumber must be less than toRandomNumber')
     }
-
+  } else {
+    console.log('startRandomNumber and toRandomNumber must be positive or equal to zero')
+  }
 }
-console.log(randomFunctionDouble(1.5,0.5,2));
+console.log(randomFunctionDouble(1.5, 0.5, 2))
+console.log(randomFunction(1.5, 0.5, 2))
