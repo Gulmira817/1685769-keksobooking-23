aab65e757c3aa837034944c7faefa6de8930a55c
Gulmira817 <38330620+Gulmira817@users.noreply.github.com>
2021-07-05 07:43:31 +0400

    task2

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 import { generateSimilarElements } from './utils/generate-similar-elements.js'
 const mapCanvas = document.querySelector('#map-canvas')
 mapCanvas.appendChild(generateSimilarElements())
-mapCanvas.appendChild(generateSimilarElements())
+
+// mapCanvas.appendChild(generateSimilarElements())
+// // const elements =
+// const form = document.querySelector('form')
+// const elementsForm = form.querySelectorAll('.ad-form__element')
+// disableField(elementsForm)
